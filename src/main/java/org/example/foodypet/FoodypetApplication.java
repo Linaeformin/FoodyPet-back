8ddae333232207bedd1b789a047d7acc6fef9a97
Linaeformin <minsa030407@gmail.com>
2026-05-12 01:09:30 +0900

    feat: 스프링 설정

---
+++ src/main/java/org/example/foodypet/FoodypetApplication.java
@@ -0,0 +1,13 @@
+package org.example.foodypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodypetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodypetApplication.class, args);
+    }
+
+}
